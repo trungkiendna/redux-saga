83db91b4e6db90f7652ee884f2c861fed9122b95
Kien NguyenTruongTrung <vuacoiam@gmail.com>
2020-05-11 17:38:58 +0700

    orgin

---
+++ src/containers/AdminHomePage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AdminHomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Đây là trang Admin</h1>
+      </div>
+    );
+  }
+}
